cb1ce03ff0b733422886ac9acfd03afb7369235c
MizanurRemon <remonmizanur@gmail.com>
2021-12-19 11:47:58 +0600

    19 Dec 2021 11:47AM

--- app/src/main/java/com/example/auxhall/MainActivity.java
+++ app/src/main/java/com/example/auxhall/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.auxhall;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton keyboardButton;
+    LinearLayout mainLayout, hideKeyBoardButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        keyboardButton = findViewById(R.id.floating_action_button);
+        mainLayout = findViewById(R.id.mainLayoutID);
+        hideKeyBoardButton = findViewById(R.id.hideKeyBoardButtonID);
+
+        keyboardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation bottomUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bottom_up);
+                mainLayout.startAnimation(bottomUp);
+                keyboardButton.setVisibility(View.GONE);
+                mainLayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+        hideKeyBoardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation bottomUp = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bottom_down);
+                mainLayout.startAnimation(bottomUp);
+                keyboardButton.setVisibility(View.VISIBLE);
+                mainLayout.setVisibility(View.GONE);
+            }
+        });
     }
 }
